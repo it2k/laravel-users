771a7fc2711cfea096045338f4990c6ef99ce651
Егор Зюскин <zyuskin_en@me.com>
2014-08-25 14:27:12 +0700

    Initial commit

---
+++ src/It2k/LaravelUsers/LaravelUsersServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace It2k\LaravelUsers;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelUsersServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('it2k/laravel-users');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
