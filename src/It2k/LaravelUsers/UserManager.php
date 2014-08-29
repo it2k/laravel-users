cc36c852c951d949f0caf0baddbe1e86487cf76f
Егор Зюскин <zyuskin_en@me.com>
2014-08-29 20:00:04 +0700

    Добавлен UserManager

---
+++ src/It2k/LaravelUsers/UserManager.php
@@ -0,0 +1,18 @@
+<?php namespace It2k\LaravelUsers;
+
+use DB;
+
+class UserManager {
+
+    /**
+     * Создание нового пользователя
+     *
+     * @param $username string
+     * @param $email string
+     * @param $password string
+     */
+    public function create($username, $email, $password)
+    {
+        //
+    }
+} 
